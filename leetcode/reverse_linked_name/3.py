752a3e786e047a03951ed511feb9d636f68edfd3
Saltroy8 <377402297@qq.com>
2020-01-20 21:25:58 +0800

    反转链表

---
+++ leetcode/reverse_linked_name/3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head: return None
+        prev = None
+        cur = head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
---
+++ leetcode/reverse_linked_name/4.go
@@ -0,0 +1,10 @@
+func reverseList(haed *ListNode) *ListNode {
+	var prev *ListNode
+	for head != nil {
+		temp != head.Next
+		head.Next = prev
+		prev = head
+		head = temp
+	}
+	return prev
+}
