763d2c257b3dde6e1b7c072ab9fbe67732d2532a
Saltroy8 <377402297@qq.com>
2020-01-20 21:33:37 +0800

    反转链表

--- leetcode/reverse_linked_name/4.go
+++ leetcode/reverse_linked_name/4.go
@@ -1,7 +1,7 @@
-func reverseList(haed *ListNode) *ListNode {
+func reverseList(head *ListNode) *ListNode {
 	var prev *ListNode
 	for head != nil {
-		temp != head.Next
+		temp := head.Next
 		head.Next = prev
 		prev = head
 		head = temp
