daab2cb62da4f9f33491ae3c5a91df17ee437fc2
Saltroy8 <377402297@qq.com>
2020-02-08 22:31:09 +0800

    大文件上传

--- big_file_upload/server/main.js
+++ big_file_upload/server/main.js
@@ -7,7 +7,7 @@ const UPLOAD_DIR = path.resolve(__dirname, ".", "target");
 server.on("request", async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
-//   res.end("hello");
+  // res.end("hello");
 
   if (req.url == '/') {
     // chunk ,  name 
