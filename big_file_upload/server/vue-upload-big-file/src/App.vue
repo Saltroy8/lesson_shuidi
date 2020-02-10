1bf18fa9cfc6b071cbb3e0b2f9916c16dbc75502
Saltroy8 <377402297@qq.com>
2020-02-10 14:21:31 +0800

    大文件上传

--- big_file_upload/server/vue-upload-big-file/src/App.vue
+++ big_file_upload/server/vue-upload-big-file/src/App.vue
@@ -4,18 +4,53 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <div>
       <input type="file" @change="handleFileChange"/>
+      <el-button @click="handleUpload">上传</el-button>
     </div>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+const Status = {
+  wait:"wait",
+  pause:"pause",
+  uploading: "uploading"
+}
+const SIZE = 0.5*1024*1024;
 
 export default {
   name: 'App',
+  data: () =>({
+    container:{ //将任务放到一起
+      file:null,
+      hash:"",
+    },
+    status:Status.wait
+  }),
   methods:{
+    async handleUpload(e) {
+      // 大量的任务
+      if(!this.container.file) return;
+      this.status = Status.uploading;
+      const fileChunkList = this.createFileChunk(this.container.file);
+      console.log(fileChunkList); 
+    },
+
+    createFileChunk(file,size = SIZE){
+      const fileChunkList = [];
+      let cur = 0;
+      while(cur < file.size) {
+        fileChunkList.push({
+          file:file.slice(cur,cur + size)
+          })
+          cur += size;
+        }
+        return fileChunkList;   
+    },
     handleFileChange(e) {
-      console.log(e.target.files);
+      const [file] = e.target.files;
+      // console.log(e.target.files);
+      this.container.file = file;
     }
   },
   components: {
