ae61f6a781609a3b69640dee2d2725aea29fb5ce
Saltroy8 <377402297@qq.com>
2020-02-11 21:06:55 +0800

    大文件上传

--- big_file_upload/vue-upload-big-file/public/hash.js
+++ big_file_upload/vue-upload-big-file/public/hash.js
@@ -1,6 +1,19 @@
+self.importScripts('/spark-md5.min.js')
+
 self.onmessage = e => {
-    self.postMessage({
-        "msg":"你好"
+    // self.postMessage({
+    //     "msg":"你好"
         
-    })
+    // })
+    const {fileChunkList} = e.data;
+    const loadNext = index => {
+        count++;
+        if(count === fileChunkList.length){
+            self.postMessage({
+
+            })
+        }
+        localNext(count);
+    }
+    loadNext(0)
 } //this 当前的线程
