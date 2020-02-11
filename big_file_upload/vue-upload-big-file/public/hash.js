2d15f43ef21b97b7e2fa3b369fd18aa1aa2b87b8
Saltroy8 <377402297@qq.com>
2020-02-11 21:32:09 +0800

    大文件上传

--- big_file_upload/vue-upload-big-file/public/hash.js
+++ big_file_upload/vue-upload-big-file/public/hash.js
@@ -6,14 +6,33 @@ self.onmessage = e => {
         
     // })
     const {fileChunkList} = e.data;
+    const spark = new self.SparkMD5.ArrayBuffer();
+    let percentage = 0;
+    let count = 0;
     const loadNext = index => {
-        count++;
-        if(count === fileChunkList.length){
-            self.postMessage({
 
-            })
+        const reader = new FileReader();
+        reader.readAsArrayBuffer(fileChunkList[index].file);
+        reader.onload = e =>{
+           count++;      
+           spark.append(e.target.result); 
+           if(count === fileChunkList.length){
+            self.postMessage({
+                percent: 100,
+                hash: spark.end()
+            });
+            self.close();
+        } else {
+            percentage += 100/fileChunkList.length;
+            self.postMessage({
+                percentage
+            });
+            loadNext(count);
         }
-        localNext(count);
+    }
+
+        
+        loadNext(count);
     }
     loadNext(0)
 } //this 当前的线程
