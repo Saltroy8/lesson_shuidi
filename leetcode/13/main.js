def422d5e131038dc916476b469ea131e66bfec4
Saltroy8 <377402297@qq.com>
2020-03-24 20:54:16 +0800

    小偷系列算法

---
+++ leetcode/13/main.js
@@ -0,0 +1,9 @@
+var massage = function(nums) {
+    var a = 0, b = 0;
+    for (var i = 0; i < nums.length; i++) {
+        var c = Math.max(b, a + nums[i]);
+        a = b;
+        b = c;
+    }
+    return b;
+};
