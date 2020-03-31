01835ff245ea2fba33b7098da76af606ee110877
Saltroy8 <377402297@qq.com>
2020-03-31 21:54:20 +0800

    数组排序

---
+++ leetcode/912/main.go
@@ -0,0 +1,12 @@
+func sortArray(nums []int) []int {
+	n := len(nums)
+for i := 1; i < n; i++ {
+	e := nums[i]
+	j := i
+	for ; j > 0 && e < nums[j-1]; j-- {
+		nums[j] = nums[j-1]
+	}
+	nums[j] = e
+}
+return nums
+}
