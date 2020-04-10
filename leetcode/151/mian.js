bd76166595bc4f44d8a8ba3f9df9dd137eeecf9e
Saltroy8 <377402297@qq.com>
2020-04-10 23:17:21 +0800

    翻转字符串里的单词

---
+++ leetcode/151/mian.js
@@ -0,0 +1,11 @@
+var reverseWords = function(s) {
+    let list = [], str = '', resStr = ''
+    s += ' '   // 加个空格 防止最后一个不是空格的情况, 确保循环到最后能把最后一个加到数组中, 省得再在下面判断
+    for(let i = 0; i < s.length; i++){
+      s[i] !==' ' ? str += s[i] : str && (list.push(str),(str = ''))
+    }
+    for(let j = list.length - 1; j >= 0; j--){
+      resStr += j !== 0 ? list[j] + ' ' : list[j]
+    }
+    return resStr
+  };
