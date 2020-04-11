60b17a0be3a49a00ab9340e8bf82afa487767b7e
Saltroy8 <377402297@qq.com>
2020-04-11 22:35:14 +0800

    鸡蛋破碎

---
+++ leetcode/887/main.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} K
+ * @param {number} N
+ * @return {number}
+ */
+let superEggDrop = (K, N)=> {
+    let dp = Array(K+1).fill(0)
+    let cnt = 0
+    while (dp[K] < N){
+        cnt++
+        for (let i=K; i>0; i--){
+            dp[i] = dp[i-1] + dp[i] + 1
+        }
+    }
+    return cnt
+};
