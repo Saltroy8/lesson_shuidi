a2fbc3e2e2ec2a54007b66c680f0b22b3614602b
Saltroy8 <377402297@qq.com>
2020-05-11 21:22:42 +0800

    Pow(x,n)

---
+++ leetcode/50/main.js
@@ -0,0 +1,17 @@
+var myPow = function (num, power) {
+    if (power < 0) return 1 / num * myPow(1 / num, -(power + 1))
+    if (power === 0) return 1 
+    if (power === 1) return num
+    // 以上分别为power小于0 等于0 等于1 的情况
+    let res = 1
+    while (power > 1) { // power大于1
+      if (power % 2 === 1) {
+        res = res * num
+        power--
+      }
+      num = num * num
+      power = power / 2
+    }
+    return res * num
+  };
+  
