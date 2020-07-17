7af233814b73dc17ee86632ef928d17fa3871bf3
Saltroy8 <377402297@qq.com>
2020-07-17 23:27:18 +0800

    搜索插入位置

---
+++ leetcode/35/1.js
@@ -0,0 +1,16 @@
+const searchInsert = (nums, target) => {
+    let lo = 0;
+    let hi = nums.length - 1;
+    while (lo <= hi) {
+      const mid = (lo + hi) >>> 1;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        hi = mid - 1;
+      } else {
+        lo = mid + 1;
+      }
+    }
+    return lo; // 退出循环时 hi比lo小1
+  };
+  
