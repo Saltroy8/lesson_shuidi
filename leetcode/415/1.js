2e3dfaf84856e76d00d09847f83f9ab99a7bde62
Saltroy8 <377402297@qq.com>
2020-08-03 22:36:24 +0800

    连接字符串

---
+++ leetcode/415/1.js
@@ -0,0 +1,14 @@
+const addStrings = (num1, num2) => {
+    let res = '';  
+    let carry = 0;
+    for (let i = num1.length - 1, j = num2.length - 1; i >= 0 || j >= 0; i--, j--) {
+      const n1 = num1[i] >= 0 ? +num1[i] : 0;
+      const n2 = num2[j] >= 0 ? +num2[j] : 0;
+      const sum = n1 + n2 + carry;
+      res = sum % 10 + res;
+      carry = sum / 10 | 0; 
+    }
+    carry && (res = '1' + res);
+    return res;
+  };
+  
