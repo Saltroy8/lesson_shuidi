440e7b31467dbddc65e08f03f12843317cbda590
Saltroy8 <377402297@qq.com>
2020-08-14 23:18:30 +0800

    有效括号

---
+++ leetcode/20/2.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    let valid = true;
+    const stack = [];
+    const mapper = {
+      "{": "}",
+      "[": "]",
+      "(": ")",
+    };
+  
+    for (let i in s) {
+      const v = s[i];
+      if (["(", "[", "{"].indexOf(v) > -1) {
+        stack.push(v);
+      } else {
+        const peak = stack.pop();
+        if (v !== mapper[peak]) {
+          return false;
+        }
+      }
+    }
+  
+    if (stack.length > 0) return false;
+  
+    return valid;
+  };
+  
