1a612dbfd7ed0b1345cc945cc9570ae1c95915ea
Saltroy8 <377402297@qq.com>
2020-08-17 22:08:22 +0800

    平衡二叉树

---
+++ leetcode/110/1.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    if(!root) return true
+    return  Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+};
+
+function height(node) {
+    if(!node) return 0
+    return 1 + Math.max(height(node.left), height(node.right)) 
+}
