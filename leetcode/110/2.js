3f7ee142dc665461a89ccffba398c4cc506d3bd1
Saltroy8 <377402297@qq.com>
2020-08-17 22:10:12 +0800

    平衡二叉树

---
+++ leetcode/110/2.js
@@ -0,0 +1,13 @@
+var isBalanced = function (root) {
+    return balanced(root) !== -1
+};
+var balanced = function (node) {
+    if (!node) return 0
+    const left = balanced(node.left)
+    const right = balanced(node.right)
+    if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
+        return -1
+    }
+    return Math.max(left, right) + 1
+}
+
