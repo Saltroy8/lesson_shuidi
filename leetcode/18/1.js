8c2f3a445e8f82a29081670f0d392d807e0f1651
Saltroy8 <377402297@qq.com>
2020-10-05 19:52:07 +0800

    四数之和

---
+++ leetcode/18/1.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    let res=[]
+    let path=[]
+    nums.sort()
+    let backtrace=(start)=>{
+        if((path.length==4)){
+            if(path[0]+path[1]+path[2]+path[3]==target){
+                res.push([...path])
+            }
+            return;
+        }
+        for(let i=start;i<nums.length;i++){
+            if(nums[i]==nums[i-1]&&i>start){
+                continue;
+            }
+            path.push(nums[i])
+            backtrace(i+1);
+            path.pop()
+        }
+    }
+    backtrace(0)
+    return res;
+};
