b5e9b770cd3e711f6306f12eac9cc85bdffa9f37
Saltroy8 <377402297@qq.com>
2020-11-23 22:51:02 +0800

    引爆气球

---
+++ leetcode/452/1.js
@@ -0,0 +1,15 @@
+var findMinArrowShots = function(points) {
+    points.sort((a, b) => a[1] - b[1]);
+    
+    let count = 0;
+    let i = 0;
+    while (i < points.length) {
+        const right = points[i][1];
+        i++;
+        while (i < points.length && points[i][0] <= right) {
+            i++
+        }
+        count++;
+    }
+    return count;
+};
