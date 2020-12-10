0dd10e27fd4ea26177a4c7efed9e992614e8d585
Saltroy8 <377402297@qq.com>
2020-12-10 23:04:26 +0800

    -

---
+++ leetcode/860/1.js
@@ -0,0 +1,24 @@
+var lemonadeChange = function(bills) {
+    var i = -1
+    var n5 = 0
+    var n10 = 0
+    while(++i < bills.length) {
+        if (bills[i] === 5) {
+            n5++
+            continue
+        } else if (bills[i] === 10) {
+            n5--
+            n10++
+        } else if (n10) {
+            n5--
+            n10--
+        } else {
+            n5 -= 3
+        }
+        if (n5 < 0) {
+            return false
+        }
+    }
+    return true
+};
+
